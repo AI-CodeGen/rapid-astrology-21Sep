ef60f982b0daa804c0ccb23b8d0ea49bec667b07
Iman Biswas <iman.biswas@sap.com>
2025-09-24 01:15:21 +0530

    Added x-request-id in frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { v4 as uuidv4 } from 'uuid';
 import { toastBridge } from '../utils/toastBridge';
 import { authBridge } from '../utils/authBridge';
 
@@ -7,19 +8,33 @@ const api = axios.create({ baseURL: '/api' });
 api.interceptors.request.use(cfg => {
   const token = localStorage.getItem('token');
   if (token) cfg.headers.Authorization = 'Bearer ' + token;
+  // Propagate / generate request correlation ID if caller didn't set one
+  if (!cfg.headers['x-request-id']) {
+    cfg.headers['x-request-id'] = (typeof crypto !== 'undefined' && crypto.randomUUID) ? crypto.randomUUID() : uuidv4();
+  }
   return cfg;
 });
 
 api.interceptors.response.use(
-  res => res,
+  res => {
+    // Attach request id onto response data meta if available (non-mutating original data shape)
+    const reqId = res.headers['x-request-id'];
+    if (reqId && res.data && typeof res.data === 'object' && !res.data.requestId) {
+      // Non-destructive augmentation (only if backend didn't already include it)
+      res.data.requestId = reqId;
+    }
+    return res;
+  },
   err => {
     if (err?.response?.status === 401) {
-      // If we still have a token locally, treat it as expired
       if (localStorage.getItem('token')) {
         authBridge.expireSession();
         toastBridge.info('Session expired. Please login again.');
       }
     }
+    // Surface request id for error handling/logging layers
+    const reqId = err?.response?.headers?.['x-request-id'];
+    if (reqId) err.requestId = reqId;
     return Promise.reject(err);
   }
 );
